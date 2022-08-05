a6a4ea22b437df5240c671f897953350046f5e75
Dwiana Kamila <dwianakas@gmail.com>
2022-08-06 06:50:13 +0700

    implement empty cart

--- src/components/cart.js
+++ src/components/cart.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
 import "../styles/cart.css";
+import { useNavigate, Link } from "react-router-dom";
 
 const Cart = ({ cart, setCart, handleChange }) => {
   const [price, setPrice] = useState(0);
+  const navigate = useNavigate();
 
   const handleRemove = (id) => {
     const arr = cart.filter((item) => item.id !== id);
@@ -16,13 +18,28 @@ const Cart = ({ cart, setCart, handleChange }) => {
     setPrice(ans);
   };
 
+  const handleShop = () =>{
+    navigate('http://localhost:3000/bookstore')
+  }
+
   useEffect(() => {
     handlePrice();
   });
 
   return (
     <article>
-      {cart.map((item) => (
+      {
+        cart.length === 0 ?
+        <>
+        <div className="cart-empty">
+            <p className="title-cart">Keranjang masih kosong</p>
+            <Link to="/bookstore"><button className="button-shop">Belanja</button></Link>
+           
+          </div>
+        </>
+        :
+        <>
+        {cart.map((item) => (
         <div className="cart_box" key={item.id}>
           <div className="cart_img">
             <img src={item.url} alt="" />
@@ -34,6 +51,8 @@ const Cart = ({ cart, setCart, handleChange }) => {
           </div>
         </div>
       ))}
+        </>
+      }
     </article>
   );
 };
